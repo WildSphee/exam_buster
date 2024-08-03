eae545a5b9be246c88daac11f4e5d2af0a1228c7
Reagan <rrr.chanhiulok@gmail.com>
2024-08-03 03:37:22 +0000

    update hkgf app

--- hkgf_app.py
+++ hkgf_app.py
@@ -32,22 +32,31 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+async def start_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued and log user info. also clears context"""
+    user: User = update.message.from_user
+    bot_response: str = f"hello❤❤ 你今日過得點呀~"
+    db._log_interaction(user, "/start", bot_response)
+
+    # clears context
+    clear_command(update, None)
+
+    await update.message.reply_text(bot_response)
+
+
 async def help_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued and log user info."""
-    user_message: str = update.message.text
     bot_response: str = "This is a simple telegram chatbot, type anything to start! /clear to clear context"
-
     user: User = update.message.from_user
-    db._log_interaction(user, user_message, bot_response)
+    db._log_interaction(user, "/help", bot_response)
     await update.message.reply_text(bot_response)
 
 
 async def clear_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     """Clear the username in the chat history."""
-    user_message: str = update.message.text
     bot_response: str = "Context cleared"
     user: User = update.message.from_user
-    db._log_interaction(user, user_message, bot_response)
+    db._log_interaction(user, "/clear", bot_response)
     db._clear_chat_history(user)
 
     await update.message.reply_text(bot_response)
@@ -75,6 +84,7 @@ def main() -> None:
     application = ApplicationBuilder().token(TOKEN).build()
 
     # Add commands
+    application.add_handler(CommandHandler("/start", start_command))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("clear", clear_command))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
